a8d86be05a9039a58b2bd0312203e9843eab3cc0
joe <jathinsomanna@gmail.com>
2025-01-22 15:45:40 +0530

    done

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.get('/status-info', (req, res) => {  
+    const statusCode = {
+        200: 'OK',
+        201: 'Created',
+        204: 'No Content',
+        400: 'Bad Request',
+        401: 'Unauthorized',
+        403: 'Forbidden',
+        404: 'Not Found',
+        405: 'Method Not Allowed',
+        429: 'Too Many Requests',
+    };
+
+    const code = parseInt(req.query.code); 
+
+    if (statusCode[code]) {
+        res.send({
+            "status": code,
+            "message": statusCode[code]
+        });
+    } else {
+        res.send({
+            "status": 400,
+            "message": "Bad Request"
+        });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
